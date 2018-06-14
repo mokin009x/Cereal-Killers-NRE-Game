9db727af5d4beae43e22509b6ac59ea1b7dbdc38
rycon2424 <rycon2424@users.noreply.github.com>
2018-06-14 11:32:54 +0200

    Push textbalon en mousehover

---
+++ Assets/MouseHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+
+    public GameObject glow;
+
+    private void Start()
+    {
+        glow.SetActive(false);
+    }
+
+    void OnMouseOver()
+    {
+        Debug.Log("Mouse is over GameObject.");
+        glow.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("Mouse is no longer on GameObject.");
+        glow.SetActive(false);
+    }
+}
